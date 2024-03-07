d099fb7fd327c2bfc8459b77782d6be317a30548
Gustavo <gustavohique12@gmail.com>
2024-03-06 23:38:12 -0300

    feat: added useMemo to Publication component

--- src/components/Publication/Publication.tsx
+++ src/components/Publication/Publication.tsx
@@ -3,7 +3,7 @@ import Story from '../Story/Story';
 import { Like, Publication as PublicationType, TypeOfPublication } from '../../types/types';
 import { formatLongData } from '../../Helper';
 
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import { SlOptionsVertical } from "react-icons/sl";
 import { FaRegHeart } from "react-icons/fa";
 import { MdOutlineModeComment } from "react-icons/md";
@@ -21,6 +21,7 @@ function Publication({ data, userIdLogged }: PublicationProps) {
     const [ isDescriptinExpanded, setIsDescriptinExpanded ] = useState(false);
     const [ myLike, setMyLike ] = useState<Like | undefined>(data.likes.find(like => like.userId === userIdLogged));
     const [ hasAds ] = useState(data.type === TypeOfPublication.ADVERTISEMENT);
+    const [ likeCounter, setLikeCounter ] = useState<number | undefined>(data?.likes?.length);
 
     const [ addLike ] = useMutation(LIKE);
     const [ removeLike ] = useMutation(DESLIKE);
@@ -30,9 +31,9 @@ function Publication({ data, userIdLogged }: PublicationProps) {
     const locationName = '';
 
     const getLikesString = () => {
-        if (data.likes) {
-            const newLikeValue = data.likes.length.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-            if (data.likes.length > 1) {
+        if (likeCounter) {
+            const newLikeValue = likeCounter.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+            if (likeCounter > 1) {
                 return newLikeValue + ' curtidas';
             } else {
                 return newLikeValue + ' curtida';
@@ -44,6 +45,8 @@ function Publication({ data, userIdLogged }: PublicationProps) {
 
     const changeLike = async () => {
         if (!!myLike) {
+            setMyLike(undefined);
+            likeCounter && setLikeCounter(likeCounter - 1);
             await removeLike({
                 variables: {
                     input: {
@@ -51,9 +54,13 @@ function Publication({ data, userIdLogged }: PublicationProps) {
                     },
                 },
             })
-            .catch((error) => console.error('Erro ao remover o like: ', error));
-            setMyLike(undefined);
+            .catch((error) => {
+                console.error('Erro ao remover o like: ', error);
+                setMyLike(myLike);
+                likeCounter && setLikeCounter(likeCounter + 1);
+            });
         } else {
+            likeCounter && setLikeCounter(likeCounter + 1);
             addLike({
                 variables: {
                     input: {
@@ -63,66 +70,73 @@ function Publication({ data, userIdLogged }: PublicationProps) {
                     },
                 },
             })
-            .then((res) => setMyLike(res.data.addPublicationLike as Like))
+            .then((res) => {
+                setMyLike(res.data.addPublicationLike as Like);
+            })
             .catch((error) => {
                 console.error('Erro ao registrar o like: ', error);
+                likeCounter && setLikeCounter(likeCounter - 1);
                 setMyLike(undefined);
             });
         }
     };
 
-    return (
-        <div className='publi-container'>
-            <div className='publi-header'>
-                <Story fromPubli={ true } />
-                <div className='user-info'>
-                    <span className='username'>{ data.username }</span>
-                    { hasAds && (<span className='location'>{ 'Patrocinado' }</span>)}
-                    { hasLocation && (<span className='location'>{ locationName }</span>)}
-                </div>
-                <div className='options'><SlOptionsVertical /></div>
-            </div>
-
-            <div className='content'>
-                { data.type === TypeOfPublication.IMAGE && <img className='image' src={ data.fileUrl } alt=""/> }
-            </div> 
-
-            <div className='icons'>
-                <span className='publi-icon' onClick={() => {changeLike()}}>
-                    { !!myLike 
-                    ? (<svg className='like' fill="currentColor" height="24" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>)
-                    : (<FaRegHeart />) } 
-                </span>
-                <span className='publi-icon'><MdOutlineModeComment /></span>
-                <div className='grow-1'>
-                    <span className='publi-icon'><LuSend /></span>
-                </div>
-                <span className='publi-icon last'><HiOutlineSave /></span>
-            </div>
-
-            <div className='comments-container'>
-                <div className='likes'>
-                    { getLikesString() } 
-                </div>
-                <div className={`description ${isDescriptinExpanded ? 'expanded': ''}`}>
-                    <span className='user'>{ data.username }</span>
-                        { data.description }
-                    { !isDescriptinExpanded && data.description && data.description?.length > 30 &&
-                        (<span className='show-more' onClick={() => setIsDescriptinExpanded(true)}>mais</span>)
-                    }
-                </div>
-                { data.comments && data.comments.length > 0 && (
-                    <div className='all-comments'>
-                        Ver { data.comments.length > 1 ? ' todos os ' + data.comments.length + ' comentários' : 'comentário' } 
+    const memoizedPublicationContent = useMemo(() => {
+        return (
+            <div className='publi-container'>
+                <div className='publi-header'>
+                    <Story fromPubli={ true } />
+                    <div className='user-info'>
+                        <span className='username'>{ data.username }</span>
+                        { hasAds && (<span className='location'>{ 'Patrocinado' }</span>)}
+                        { hasLocation && (<span className='location'>{ locationName }</span>)}
                     </div>
-                )}
-                {/* <div className='preview-comment'> </div> */}
-                <div className='publi-time'>
-                    { formatLongData(data.dateTime) }
+                    <div className='options'><SlOptionsVertical /></div>
+                </div>
+
+                <div className='content'>
+                    { data.type === TypeOfPublication.IMAGE && <img className='image' src={ data.fileUrl } alt=""/> }
+                </div> 
+
+                <div className='icons'>
+                    <span className='publi-icon' onClick={() => {changeLike()}}>
+                        { !!myLike 
+                        ? (<svg className='like' fill="currentColor" height="24" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg>)
+                        : (<FaRegHeart />) } 
+                    </span>
+                    <span className='publi-icon'><MdOutlineModeComment /></span>
+                    <div className='grow-1'>
+                        <span className='publi-icon'><LuSend /></span>
+                    </div>
+                    <span className='publi-icon last'><HiOutlineSave /></span>
+                </div>
+
+                <div className='comments-container'>
+                    <div className='likes'>
+                        { getLikesString() } 
+                    </div>
+                    <div className={`description ${isDescriptinExpanded ? 'expanded': ''}`}>
+                        <span className='user'>{ data.username }</span>
+                            { data.description }
+                        { !isDescriptinExpanded && data.description && data.description?.length > 30 &&
+                            (<span className='show-more' onClick={() => setIsDescriptinExpanded(true)}>mais</span>)
+                        }
+                    </div>
+                    { data.comments && data.comments.length > 0 && (
+                        <div className='all-comments'>
+                            Ver { data.comments.length > 1 ? ' todos os ' + data.comments.length + ' comentários' : 'comentário' } 
+                        </div>
+                    )}
+                    {/* <div className='preview-comment'> </div> */}
+                    <div className='publi-time'>
+                        { formatLongData(data.dateTime) }
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        );
+    }, [data, userIdLogged, myLike, hasAds, isDescriptinExpanded, likeCounter]);
+
+    return memoizedPublicationContent;
 }
 
 export default Publication;
